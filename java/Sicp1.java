c9af3deb4d9cb482cf425d584c7f93d953c72354
Orlando Karam <orlando.karam@gmail.com>
2016-03-30 22:06:23 -0400

    Starting with 1.2

--- java/Sicp1.java
+++ java/Sicp1.java
@@ -23,7 +23,8 @@ class Sicp1_x {
 }
 
 class CubicRoot {
-        static BiFunction<Double,Double,Double> sqrtIter;
+    // java does not support recursive lambdas :( so we have to cheat
+    static BiFunction<Double,Double,Double> sqrtIter;
     
     public static double cubicRoot(Double xx) {
         Function<Double,Double> square      = (x)->x*x;
@@ -31,6 +32,7 @@ class CubicRoot {
         BiFunction<Double,Double,Double> average         = (x, y) -> (x+y)/2;
         BiFunction<Double,Double,Boolean> isGoodEnough    = (guess, x) -> Math.abs(x-cube.apply(guess))<=0.001;
         BiFunction<Double,Double,Double> improveGuess    = (guess, x) -> (x/square.apply(guess)+2*guess)/3 ;
+        
         sqrtIter = (guess,  x) -> isGoodEnough.apply(guess,x) ? guess : sqrtIter.apply( improveGuess.apply(guess,x), x);
         return sqrtIter.apply(1.0,xx);        
     }
---
+++ racket/sicp-1.2.scm
@@ -0,0 +1,22 @@
+(define (factorial2 n)
+  (if (= n 1)
+      1
+      (* n (factorial2 (- n 1)))))
+
+
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+
+(define (factorial n)
+  (fact-iter 1 1 n))
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
